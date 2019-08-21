abeddf9ac519de3d398cac0d301c8029c4bc9940
Pankaj Anadure <47429432+pankajanadure@users.noreply.github.com>
2019-08-21 10:51:15 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -2,14 +2,14 @@
  cd Register_Frontend/build && start python -m http.server 8006
  start java -jar UserConfirmation_Backend/target/UserConfirmation_Backend.jar
  cd ../../UserConfirmation_Frontend/build && start python -m http.server 8002
- REM start java -jar Forgotpassword_Backend/target/Forgotpassword_Backend.jar
- REM cd ../../forgotpassword_frontend/build && start python -m http.server 8010
- REM start java -jar DataRetrievalService_BackEnd/target/DataRetrievalService_BackEnd.jar
- REM start java -jar LoginService_BackEnd/target/LoginService_BackEnd.jar
- REM cd ../../LoginService_Frontend/build && start python -m http.server 8014
- REM start java -jar ChangePasswordService_Backend/target/ChangePasswordService_Backend.jar
- REM cd ../../ChangePasswordService_Frontend/build && start python -m http.server 8016
- REM start java -jar Edit_Profile_Backend/target/Edit_Profile_Backend.jar
- REM cd ../../Edit_Profile_Frontend/build && start python -m http.server 8019
- REM start java -jar Admin_Backend/target/Admin_Backend.jar
- REM cd ../../Admin_Frontend/build && start python -m http.server 8021
+ start java -jar Forgotpassword_Backend/target/Forgotpassword_Backend.jar
+ cd ../../forgotpassword_frontend/build && start python -m http.server 8010
+ start java -jar DataRetrievalService_BackEnd/target/DataRetrievalService_BackEnd.jar
+ start java -jar LoginService_BackEnd/target/LoginService_BackEnd.jar
+ cd ../../LoginService_Frontend/build && start python -m http.server 8014
+ start java -jar ChangePasswordService_Backend/target/ChangePasswordService_Backend.jar
+ cd ../../ChangePasswordService_Frontend/build && start python -m http.server 8016
+ start java -jar Edit_Profile_Backend/target/Edit_Profile_Backend.jar
+ cd ../../Edit_Profile_Frontend/build && start python -m http.server 8019
+ start java -jar Admin_Backend/target/Admin_Backend.jar
+ cd ../../Admin_Frontend/build && start python -m http.server 8021
