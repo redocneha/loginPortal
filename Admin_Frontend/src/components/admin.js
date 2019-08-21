def5afac6b670f05d818edaf6e2cbf3c885d9b65
Maitreyee Gadwe <maigadwe@publicisgroupe.net>
2019-08-21 18:30:05 +0530

    Sprint 21/08/19 Update2

--- Admin_Frontend/src/components/admin.js
+++ Admin_Frontend/src/components/admin.js
@@ -1,47 +1,64 @@
-import React from 'react';
+import React,{Component} from 'react';
 import MaterialTable from 'material-table';
+import axios from 'axios';
 
-export default function Admin() {
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'FirstName', field: 'firstname' },
-      { title: 'Last Name', field: 'lastname' },
-      { title: 'Email', field: 'email', type: 'string' },
-      { title: 'Phone',field: 'phone',type:'numeric'},
-    ],
-    data: [
-      { firstname: 'Mehmet', lastname: 'Baran', email: 'eanj@mial.com', phone: 9887656666 },
-      { firstname: 'chaman', lastname: 'tappu', email: 'dsdeanj@mial.com', phone: 988765966 },
-      { firstname: 'naman', lastname: 'hoodi', email: 'eadsdnj@mial.com', phone: 9887656866 },
-      { firstname: 'kalu', lastname: 'pvr', email: 'eadsnj@mial.com', phone: 988765666 },
-      { firstname: 'pappu', lastname: 'data', email: 'eadsdnj@mial.com', phone: 988765666 },
-      { firstname: 'raja', lastname: 'name', email: 'eandsj@mial.com', phone: 988765466 },
-      { firstname: 'bheem', lastname: 'cuh bhi', email: 'eanaaj@mial.com', phone: 9887656666 },
-      { firstname: 'modi', lastname: 'ji', email: 'eanwwj@mial.com', phone: 9887652666 },
-    ],
-  });
 
-  function handle(event,rowData){
+
+export default class Admin extends Component {
+
+    constructor(){
+        super();
+        //this.fun=this.fun.bind(this);
+        this.state={
+            columns:[
+              { title: 'FirstName', field: 'fname' },
+              { title: 'Last Name', field: 'lname' },
+              { title: 'Email', field: 'email', type: 'string' },
+              { title: 'Phone',field: 'phone',type:'numeric'},
+            ],
+            data:[]
+        }
+         
+    }
+
+    componentDidMount(){
+      axios.get('http://localhost:8020/getAll')
+        .then(res => {
+          this.setState({
+            data:res.data
+          });
+
+          console.log(res.data);
+        })
+     }
+
+    handle(event,rowData){
     console.log(rowData);
   }
 
-  return (
-    <MaterialTable
-      title="User List"
-      columns={state.columns}
-      data={state.data}
-      //onRowClick={(event,data)=> handle(event,data)}
-      options={{
-                actionsColumnIndex:4,
-              }}
-      actions={[
-          {
-            title:'Edit',
-            icon: 'edit',
-            tooltip: 'Edit Profile',
-            onClick: (event, rowData) => handle(event,rowData)
-          }
-        ]}
-    />
-  );
+ 
+
+    render(){
+        console.log(this);
+        return  (
+            <MaterialTable
+              title="User List"
+              columns={this.state.columns}
+              data={this.state.data}
+              //onRowClick={(event,data)=> handle(event,data)}
+              options={{
+                        actionsColumnIndex:4,
+                      }}
+              actions={[
+                  {
+                    title:'Edit',
+                    icon: 'edit',
+                    tooltip: 'Edit Profile',
+                    onClick: (event, rowData) => this.handle(event,rowData)
+
+                  }
+                ]}
+            />
+        );
+    }
 }
